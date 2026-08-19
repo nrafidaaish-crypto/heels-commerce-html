a2d323e846fb90e077839469213a12a812be590e
nrafidaaish-crypto <nrafidaaish@gmail.com>
2026-08-19 10:54:44 +0700

    Update login.js

--- login.js
+++ login.js
@@ -41,12 +41,12 @@ function handleLogin(e) {
   const p = document.getElementById('login-password').value.trim();
 
   if (selectedRole === 'admin') {
-    if (u === 'Hiraya Georgienne' && p === 'prettyheelsSt' && email === 'hirayagienne@gmail.com' && phone === '082839103746') {
+    if (u === 'Hiraya Georgienne' && p === 'prettyheels' && email === 'hirayagienne@gmail.com' && phone === '0828') {
       currentUser = { 
         role: 'admin', 
-        name: 'Hiraya Georgienne',
+        name: 'hiraya georgienne',
         email: 'hirayagienne@gmail.com',
-        phone: '082839103746'
+        phone: '0828'
       };
       showWelcomeScreen();
     } else {
